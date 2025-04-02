fb5006eef95244845a50570de92a8dd72e92ee22
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-04-02 15:20:47 +0300

    added header and header styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Lucky Word Game</div>;
+  return <div></div>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { CgMenu } from "react-icons/cg";
+import { CgProfile } from "react-icons/cg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex align-center justify-between p-6 border-10 border-transparent border-gradient">
+      <div>
+        <button>
+          <CgMenu className="size-7 text-blue-800 hover:text-blue-500 cursor-pointer" />
+        </button>
+      </div>
+      <div>
+        <span className="text-2xl font-bold neon-text cursor-pointer">
+          Lucky Word Game
+        </span>
+      </div>
+      <div>
+        <button>
+          <CgProfile className="size-7 text-blue-800 hover:text-blue-500 cursor-pointer" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
