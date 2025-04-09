73756a70589c6c4b625934558323745437d238fe
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-04-09 23:18:20 +0300

    data was got by category

--- src/app/gameBoard/[category]/page.tsx
+++ src/app/gameBoard/[category]/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import React, { useState } from "react";
 import words from "../../../constants/words.json";
 
 const gameBoard = () => {
   const params = useParams();
-  const currentCategory = params?.category as string;
-  const [questionsByCategory, setQuestionsByCategory] = useState([]);
+  const category = params?.category as string;
+
+  const normalizeCategory = (str: string) =>
+    str.toLowerCase().replace(/[^a-z0-9]/gi, "");
+
+  const currentCategory = Object.keys(words.categories).find(
+    (key) => normalizeCategory(key) === category
+  ) as keyof typeof words.categories | undefined;
+
+  const questions = currentCategory ? words.categories[currentCategory] : [];
 
   return (
     <div className="flex h-screen justify-center items-center drop-shadow-[0_4px_4px_yellow]">
@@ -16,7 +23,9 @@ const gameBoard = () => {
 "
       >
         <div>
-          <h1 className="neon-text text-2xl font-bold">{currentCategory}</h1>
+          <h1 className="neon-text text-2xl font-bold cursor-pointer">
+            {currentCategory}
+          </h1>
         </div>
         <div></div>
       </div>
--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -4,20 +4,20 @@ import words from "../constants/words.json";
 export default function CategoryList() {
   const categoryTitles = Object.keys(words.categories);
 
+  const normalizeCategory = (str: string) =>
+    str.toLowerCase().replace(/[^a-z0-9]/gi, "");
+
   return (
     <div className="flex flex-col justify-center items-center">
-      <h1 className=" neon-text text-xl font-bold mb-4 m-5">Categories</h1>
+      <h1 className=" neon-text text-2xl font-bold mb-4 m-5">Categories</h1>
       <ul className="grid grid-cols-2  gap-4 max-w-full overflow-hidden">
         {categoryTitles.map((title, index) => (
           <li key={index}>
-            <Link
-              href={`/gameBoard/${title
-                .toLocaleLowerCase()
-                .trim()
-                .replace(/\s+/g, "")}`}
-            >
+            <Link href={`/gameBoard/${normalizeCategory(title)}`}>
               <button className="neon-button-border min-w-80 m-4  p-2">
-                <span className="neon-text">{title}</span>
+                <span className="neon-text cursor-pointer font-bold">
+                  {title}
+                </span>
               </button>
             </Link>
           </li>
