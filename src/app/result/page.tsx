2ff7976e23dd79c735b7396ed89091cd418008f2
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-04-23 19:09:56 +0300

    created result page

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Result = () => {
-  return <div>Result</div>;
+  return (
+    <div className="flex h-screen justify-center items-center drop-shadow-[0_4px_4px_yellow]">
+      <div
+        className="gameBoard-border flex flex-col items-center  p-5 w-3/4 h-128 
+"
+      >
+        <h1 className="neon-text font-bold text-3xl">Result</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Result;
