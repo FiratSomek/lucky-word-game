63fb9007de1755e8fe0ac9bb4127ed50ac0579e7
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-04-29 22:31:23 +0300

    created page for last games

---
+++ src/app/lastGamesPage/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LastGamesPage = () => {
+  return <div>LastGamesPage</div>;
+};
+
+export default LastGamesPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,14 @@ export default function Home() {
         </Link>
       </div>
       <div>
-        <button className="neon-button-border cursor-pointer p-4 mt-20 text-lg font-bold drop-shadow-xl">
-          <span className="neon-text">Show Last 5 Game Results</span>
-        </button>
+        <Link
+          href={`/lastGamesPage
+          `}
+        >
+          <button className="neon-button-border cursor-pointer p-4 mt-20 text-lg font-bold drop-shadow-xl">
+            <span className="neon-text">Show Last 5 Game Results</span>
+          </button>
+        </Link>
       </div>
     </div>
   );
