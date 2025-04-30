336a567d7973d9941978422d6505a25ef5326686
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-04-30 23:49:09 +0300

    added css styles of last 5 games page

--- src/app/lastGamesPage/page.tsx
+++ src/app/lastGamesPage/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const LastGamesPage = () => {
-  return <div>LastGamesPage</div>;
+  return (
+    <div className="flex h-screen justify-center items-center drop-shadow-[0_4px_4px_yellow]">
+      <div
+        className="gameBoard-border flex flex-col items-center  p-5 w-3/4 h-128 
+"
+      >
+        <h1 className="neon-text text-2xl font-bold ">Last 5 games</h1>
+      </div>
+    </div>
+  );
 };
 
 export default LastGamesPage;
